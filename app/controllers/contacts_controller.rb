5d53ce36c33b6d9a542f285cb95a6a65d686a4f6
Billy Taylor <billytaylor.nz@gmail.com>
2015-04-27 01:45:21 +0000

    Added flash notices to contact page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,21 @@ class ContactsController < ApplicationController
   end
   
   def create 
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      flash[:success] = 'Message sent.'
+      redirect_to new_contact_path
+      
+    else @contact.save
+      flash[:danger] = 'Message not sent - Please make sure all fields are completed.'
+      redirect_to new_contact_path
+    end
+   end
+    
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
   end
-end
+end 
+
